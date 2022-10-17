f7bfa956a8064b2d511c3434791b445429ff019e
Paulo Henrique (PH) <ppaulo_hh@hotmail.com>
2022-10-17 15:08:15 -0300

    initial commit
    submiting initial commit with files generated from https://start.spring.io/

---
+++ authTST/src/main/java/com/example/authTST/AuthTstApplication.java
@@ -0,0 +1,13 @@
+package com.example.authTST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTstApplication.class, args);
+	}
+
+}
---
+++ authTST/src/test/java/com/example/authTST/AuthTstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authTST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
